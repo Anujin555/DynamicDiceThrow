10ccf99f1c91149271899bec8f308681bfb58613
Your Name <ikhbayranujin@gmail.com>
2025-11-03 16:23:31 -0500

    fixed

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,11 +2,25 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
 
+/*
+Our DieThrow application has been refactored to move the dieRoll() logic
+into the ViewModel instead of the Fragment.
+Study the DieViewModel, ButtonFragment, and DieFragment classes to
+see the changes.
+
+Follow the requirements below to have this app function
+in both portrait and landscape configurations.
+The Activity layout files for both Portrait and Landscape are already provided
+*/
+
+class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     private var hasTwoColumns = false
     private lateinit var dieViewModel: DieViewModel
 
@@ -15,26 +29,41 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         setContentView(R.layout.activity_main)
 
         hasTwoColumns = findViewById<View>(R.id.container2) != null
+
         dieViewModel = ViewModelProvider(this)[DieViewModel::class.java]
+        val buttonFragment = ButtonFragment()
+        val dieFragment = DieFragment()
 
         if (savedInstanceState == null) {
             if (!hasTwoColumns) {
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.container1, ButtonFragment())
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container1, buttonFragment)
                     .commit()
             }
-            else {
-                supportFragmentManager.beginTransaction()
-                    .add(R.id.container1, ButtonFragment())
-                    .add(R.id.container2, DieFragment())
+            if (hasTwoColumns) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1, buttonFragment)
+                    .add(R.id.container2, dieFragment)
                     .commit()
             }
         }
 
         dieViewModel.getDieRoll().observe(this) {
             if (!hasTwoColumns) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.container1, DieFragment())
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1, dieFragment)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .commit()
+            }
+            if (hasTwoColumns) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.container1, buttonFragment)
+                    .replace(R.id.container2, dieFragment)
                     .setReorderingAllowed(true)
                     .addToBackStack(null)
                     .commit()
@@ -43,8 +72,12 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         }
     }
 
-
     override fun buttonClicked() {
         dieViewModel.rollDie()
     }
+
+    // This callback function gets invoked when the child Fragment invokes it
+        // Remember to place Fragment transactions on BackStack so then can be reversed
+
 }
+
